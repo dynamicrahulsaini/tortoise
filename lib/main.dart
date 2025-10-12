889315f3a34a45630eb77ff8e56767ebbb09ca7a
Rahul <41207152+dynamicrahulsaini@users.noreply.github.com>
2025-10-12 15:21:29 +0530

    updated app theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'core/navigation/navigation_service.dart';
+import 'core/theme/app_colors.dart';
+import 'features/product_catalog/pages/product_catalog_screen.dart';
+import 'features/product_catalog/bloc/product_catalog_bloc.dart';
+import 'features/product_catalog/bloc/product_catalog_event.dart';
+import 'features/product_catalog/repositories/product_catalog_repository.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<ProductCatalogBloc>(
+          create: (context) => ProductCatalogBloc(
+            ProductCatalogRepository(),
+          )..add(const LoadProducts()),
         ),
+      ],
+      child: MaterialApp(
+        title: 'Marketplace App',
+        navigatorKey: NavigationService.navigatorKey,
+        theme: ThemeData(
+            fontFamily: 'HafferXH',
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: AppColors.primary,
+              brightness: Brightness.light,
+            ),
+            useMaterial3: true,
+            scaffoldBackgroundColor: AppColors.black1,
+            appBarTheme: const AppBarTheme(
+              backgroundColor: AppColors.black1,
+              scrolledUnderElevation: 0.0,
+              surfaceTintColor: Colors.transparent,
+            )),
+        home: const ProductCatalogScreen(),
       ),
     );
   }
